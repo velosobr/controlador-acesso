64665f7140b1cc8a6494a0610148fa37ee73c943
Linnety3 <linoc.veloso@gmail.com>
2019-05-07 01:01:19 -0300

    first Commit

---
+++ ControleAcesso/src/br/ufsc/ine5605/controleacesso/ControleAcesso.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufsc.ine5605.controleacesso;
+
+import br.ufsc.ine5605.controleacesso.Controller.CtrlPrincipal;
+
+/**
+ *
+ * @author Linnety3
+ */
+public class ControleAcesso {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        CtrlPrincipal controle = new CtrlPrincipal();
+        
+        controle.inicio();        
+    }
+
+}
---
+++ ControleAcesso/src/br/ufsc/ine5605/controleacesso/Controller/CtrlPrincipal.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufsc.ine5605.controleacesso.Controller;
+
+/**
+ *
+ * @author Linnety3
+ */
+public class CtrlPrincipal {
+
+    public void inicio() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
