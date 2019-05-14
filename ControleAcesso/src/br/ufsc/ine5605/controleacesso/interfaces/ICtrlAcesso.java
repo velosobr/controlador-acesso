3f1e533ce21d9b1b7660f9d4cfcc1714cba8daff
Linnety3 <linoc.veloso@gmail.com>
2019-05-14 12:48:04 -0300

    implementação da interface de controlador de acesso

--- ControleAcesso/src/br/ufsc/ine5605/controleacesso/interfaces/ICtrlAcesso.java
+++ ControleAcesso/src/br/ufsc/ine5605/controleacesso/interfaces/ICtrlAcesso.java
@@ -5,10 +5,31 @@
  */
 package br.ufsc.ine5605.controleacesso.interfaces;
 
+import br.ufsc.ine5605.controleacesso.Model.Acesso;
+
 /**
  *
  * @author Linnety3
  */
 public interface ICtrlAcesso {
+
+    /**
+     * Um dos principais métodos do sistema, onde ele vai verificar se o 
+     * @param matricula - matricula do aluno a ser liberado
+     * @param codigoSala - codigo da sala que vai receber o acesso do aluno
+     * @return deve
+     */
+    public boolean ehLiberadoAcesso(int matricula, String codigoSala);
+
+    public void listaAcesso();
+
+    public String geraLogByMatricula(int matricula);
+
+    public String geraLogByCodigoSala(String codigoSala);
+    
+    public Acesso findAcessoByMatricula(int matricula);
+    
+    public Acesso findAcessoByCodigoSala(String codigoSala);
+    
     
 }
