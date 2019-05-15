5895aa4c0800fc6f9973f4aa8e75a007d2514f20
caionoguerol <caionoguerol@gmail.com>
2019-05-15 17:20:27 -0300

    Metodos add e del sala incluidos

--- ControleAcesso/src/br/ufsc/ine5605/controleacesso/Controller/CtrlSala.java
+++ ControleAcesso/src/br/ufsc/ine5605/controleacesso/Controller/CtrlSala.java
@@ -6,31 +6,50 @@
 package br.ufsc.ine5605.controleacesso.Controller;
 
 import br.ufsc.ine5605.controleacesso.Model.Sala;
+import br.ufsc.ine5605.controleacesso.View.TelaSala;
 import br.ufsc.ine5605.controleacesso.interfaces.ICtrlSala;
+import java.util.ArrayList;
 
 /**
  *
  * @author Linnety3
  */
 public class CtrlSala implements ICtrlSala {
-
+    
+    private final CtrlPrincipal ctrlPrincipal;
+    private TelaSala telaSala;
+    private ArrayList <Sala> salas;
+    
     public CtrlSala(CtrlPrincipal ctrlPrincipal) {
-        
+        this.ctrlPrincipal = ctrlPrincipal;
+        this.telaSala = new TelaSala(this);
+        this.salas = new ArrayList<>();
     }
 
     @Override
     public void addSala(String codigoSala, int numero, char bloco, String centro, String campus) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Sala salaParaVerificar = findSalaByCodigoSala(codigoSala);
+        Sala salaParaIncluir = null;
+        if(salaParaVerificar == null){
+            salaParaIncluir = new Sala(codigoSala, numero, bloco, centro, campus);
+            salas.add(salaParaIncluir);
+        }
     }
 
     @Override
     public void delSala(String codigoSala) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Sala salaParaDeletar = findSalaByCodigoSala(codigoSala);
+        salas.remove(salaParaDeletar);
     }
 
     @Override
     public Sala findSalaByCodigoSala(String codigoSala) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        for(Sala sala:salas){
+            if(sala.getCodigoSala().equals(codigoSala)){
+                return sala;
+            }
+        }
+        return null;
     }
 
     @Override
