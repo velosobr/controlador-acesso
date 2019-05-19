7cda9c8d54a61e246edc6f46e6353e6c25a42d39
Linnety3 <linoc.veloso@gmail.com>
2019-05-18 23:58:36 -0300

    tela pessoa - cadastro de aluno

--- ControleAcesso/src/br/ufsc/ine5605/controleacesso/View/TelaPessoa.java
+++ ControleAcesso/src/br/ufsc/ine5605/controleacesso/View/TelaPessoa.java
@@ -34,20 +34,48 @@ public class TelaPessoa {
         System.out.println(" ");
         try {
 
-            System.out.println("1 ");
-            System.out.println("2 ");
-            System.out.println("9 - Sair");
+            System.out.println("1 - Incluir aluno");
+            System.out.println("2 - Incluir servidor");
+            System.out.println("3 - Deletar pessoa no sistema");
+            System.out.println("4 - Cadastrar acesso à sala");
+            System.out.println("5 - Deletar acesso à sala");
+            System.out.println("6 - Listar salas cadastras na pessoa");
+            System.out.println("7 - Encontrar pessoa pela matrícula");
+            System.out.println("99 - Sair");
 
             int opcao = validador.recebeValorInteiro("--- Escolha uma das opcoes acima e tecle enter. ---");
             switch (opcao) {
                 case (1):
-
+                    System.out.println("---");
+                    System.out.println("Informações de cadastro do aluno");
+                    System.out.println("---");
+                    int matricula = validador.recebeValorInteiro("Digite a matricula");
+                    String nome = validador.recebeValorString("Digite o nome do aluno");
+                    int telefone = validador.recebeValorInteiro("Digite o telefone do aluno sem caracteres especiais. Ex: 984841234");
+                    String email = validador.recebeValorString("Digite o email do aluno");
+                    String curso = validador.recebeValorString("Digite o curso que o aluno pertence");
+                    ctrlPessoa.incluiAluno(matricula, nome, telefone, email, curso);
                     break;
 
                 case (2):
 
                     break;
-                case (9):
+                case (3):
+
+                    break;
+                case (4):
+
+                    break;
+                case (5):
+
+                    break;
+                case (6):
+
+                    break;
+                case (7):
+
+                    break;
+                case (99):
                     System.exit(0);
 
                 default:
