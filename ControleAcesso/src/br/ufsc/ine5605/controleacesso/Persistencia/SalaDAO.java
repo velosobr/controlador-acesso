1f319f2d9ba05ef84409262e96a919e6959ac14b
Lino <linoc.veloso@gmail.com>
2019-06-10 23:28:21 -0300

    Correção do projeto comp persistencia

--- ControleAcesso/src/br/ufsc/ine5605/controleacesso/Persistencia/SalaDAO.java
+++ ControleAcesso/src/br/ufsc/ine5605/controleacesso/Persistencia/SalaDAO.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.ufsc.ine5605.controleacesso.PersistenciaLino;
+package br.ufsc.ine5605.controleacesso.Persistencia;
 
 import br.ufsc.ine5605.controleacesso.Model.Sala;
 import com.sun.imageio.spi.FileImageOutputStreamSpi;
