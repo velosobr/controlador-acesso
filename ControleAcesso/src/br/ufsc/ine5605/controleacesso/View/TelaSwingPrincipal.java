9651b9ebfcc15f13c4f27c1d722a922e9ca708ac
Lino <linoc.veloso@gmail.com>
2019-06-18 01:54:10 -0300

    implementando tela com Jpanel

--- ControleAcesso/src/br/ufsc/ine5605/controleacesso/View/TelaSwingPrincipal.java
+++ ControleAcesso/src/br/ufsc/ine5605/controleacesso/View/TelaSwingPrincipal.java
@@ -7,16 +7,19 @@ package br.ufsc.ine5605.controleacesso.View;
 
 import br.ufsc.ine5605.controleacesso.Controller.CtrlPrincipal;
 import br.ufsc.ine5605.controleacesso.Model.Servidor;
-import java.awt.Container;
-import java.awt.FlowLayout;
+import java.awt.Color;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.border.BevelBorder;
 
 /**
  *
@@ -25,48 +28,69 @@ import javax.swing.JOptionPane;
 public class TelaSwingPrincipal extends JFrame {
 
     private static TelaSwingPrincipal instancia;
-    
-    private final GridBagLayout layout; // layout desse frame
-    private GridBagConstraints constraints;
     private JLabel label;
     private JButton botaoUm;
     private JButton botaoDois;
 
     public TelaSwingPrincipal() {
+
         super("Controlador de acesso");
-        layout = new GridBagLayout();
-        setLayout(layout);
+
+        // create a new panel with GridBagLayout manager
+        JPanel panelPrincipal = new JPanel(new GridBagLayout());
+
+        GridBagConstraints constraintsPanel = new GridBagConstraints();
+        constraintsPanel.anchor = GridBagConstraints.WEST;
+        constraintsPanel.insets = new Insets(10, 10, 10, 10);
+        panelPrincipal.setBackground(Color.white);
+
+        panelPrincipal.setBorder(BorderFactory.createTitledBorder(
+                BorderFactory.createEtchedBorder(), "Controle de acesso"));
+
+        setSize(315, 210);
+
+        setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(315 ,210);
-        
-        Container container = getContentPane();
-        container.setLayout(new FlowLayout());
+        getContentPane().add(panelPrincipal);
 
-        label = new JLabel();
+        GridBagConstraints constraints = new GridBagConstraints(); // instancia restrições
+
+        //Abrir porta 
+        // botão 1
         botaoUm = new JButton();
-        botaoDois = new JButton();
-
-        label.setText("Bem vindo ao sistema!!!");
-        //Abrir porta
         botaoUm.setText("Abrir porta");
+
+        constraints.weightx = 0.5;
+        constraints.gridx = 0;
+        constraints.gridy = 1;
+        constraints.ipadx = 52;
+        constraints.ipady = 20;
+
+        panelPrincipal.add(botaoUm, constraints);
         botaoUm.setActionCommand("1");
 
         //Abrir Tela Admm
-        botaoDois.setText("Botão Dois");
+        //botão 2
+        botaoDois = new JButton();
+        botaoDois.setText("Abrir Tela ADM");
+
+        constraints.weightx = 0.5;
+        constraints.gridx = 0;
+        constraints.gridy = 2;
+        constraints.insets = new Insets(10, 0, 0, 0);
+        constraints.ipadx = 30;
+        constraints.ipady = 20;
+        panelPrincipal.add(botaoDois, constraints);
         botaoDois.setActionCommand("2");
-        
+
         GerenciadorBotoes btManager = new GerenciadorBotoes();
+
         botaoUm.addActionListener(btManager);
+
         botaoDois.addActionListener(btManager);
 
-        container.add(label);
-        container.add(botaoUm);
-        container.add(botaoDois);
-
-        setSize(360, 250);
         setLocationRelativeTo(null);
 
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
     public static TelaSwingPrincipal getInstancia() {
