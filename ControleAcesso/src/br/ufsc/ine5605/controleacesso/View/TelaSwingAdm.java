9d857f7231488beed19fd3ec65778b2bcf27d181
Lino <linoc.veloso@gmail.com>
2019-06-20 13:10:33 -0300

    implementação do botão boltar

--- ControleAcesso/src/br/ufsc/ine5605/controleacesso/View/TelaSwingAdm.java
+++ ControleAcesso/src/br/ufsc/ine5605/controleacesso/View/TelaSwingAdm.java
@@ -29,11 +29,11 @@ public class TelaSwingAdm extends JFrame {
         //create a new panel with GBL manager
         JPanel panelADM = new JPanel(new GridBagLayout());
         GridBagConstraints constraintsPanel = new GridBagConstraints(); // instancia restrições
-
+       
         constraintsPanel.anchor = GridBagConstraints.WEST;
         constraintsPanel.insets = new Insets(10, 10, 10, 10);
         panelADM.setBackground(Color.WHITE);
-          setSize(315, 250);
+        setSize(315, 280);
 
         panelADM.setBorder(BorderFactory.createTitledBorder(
                 BorderFactory.createEtchedBorder(), "Tela ADM"));
@@ -51,7 +51,7 @@ public class TelaSwingAdm extends JFrame {
         constraints.insets = new Insets(10, 0, 0, 0);
         constraints.gridx = 0;
         constraints.gridy = 0;
-        constraints.ipadx = 65;
+        constraints.ipadx = 40;
         constraints.ipady = 20;
 
         panelADM.add(btnSala, constraints);
@@ -63,7 +63,7 @@ public class TelaSwingAdm extends JFrame {
         constraints.weightx = 0.5;
         constraints.gridx = 0;
         constraints.gridy = 1;
-        constraints.ipadx = 48;
+        constraints.ipadx = 20;
         constraints.ipady = 20;
 
         panelADM.add(btnPessoa, constraints);
@@ -71,23 +71,35 @@ public class TelaSwingAdm extends JFrame {
         //Botão Acessos
         JButton btnAcesso = new JButton("Gerenciador de Acessos");
         btnAcesso.setActionCommand("3");
-        constraints.insets = new Insets(10, 0, 10, 0);
+        constraints.insets = new Insets(10, 0, 0, 0);
         constraints.weightx = 0.5;
         constraints.gridx = 0;
         constraints.gridy = 2;
-        constraints.ipadx = 48;
+        constraints.ipadx = 20;
         constraints.ipady = 20;
 
         panelADM.add(btnAcesso, constraints);
+
+        //Botão Voltar
+        JButton btnVoltar = new JButton("Voltar");
+        btnVoltar.setActionCommand("4");
+        constraints.insets = new Insets(10, 10, 10, 10);
+        constraints.weightx = 0.5;
+        constraints.gridx = 0;
+        constraints.gridy = 3;
+        constraints.ipadx = 125;
+        constraints.ipady = 20;
+        
+
+        panelADM.add(btnVoltar, constraints);
 
         //gerenciador dos botões
         GerenciadorBotoes btManager = new GerenciadorBotoes();
         btnSala.addActionListener(btManager);
         btnPessoa.addActionListener(btManager);
         btnAcesso.addActionListener(btManager);
-        
-        
-      
+        btnVoltar.addActionListener(btManager);
+
     }
 
     public static TelaSwingAdm GetInstacia() {
@@ -101,29 +113,42 @@ public class TelaSwingAdm extends JFrame {
         return CtrlPrincipal.getInstancia();
     }
 
-    /**
-     * public void inicio() throws IllegalArgumentException,
-     * InputMismatchException { System.out.println("--- TELA ADM - GERENCIAL
-     * ---"); System.out.println( " "); System.out.println("1 - Cadastro de
-     * pessoas "); System.out.println("2 - Cadastro de salas ");
-     * System.out.println("3 - Listar acessos "); System.out.println("4 - Voltar
-     * ao menu principal"); System.out.println("");
-     *
-     * try { int opcao = validador.recebeValorInteiro( "Escolha a opcao
-     * desejada, insira o numero e tecle enter : ---");
-     *
-     * switch (opcao) { case (1):
-     * CtrlPrincipal.getInstancia().getCtrlPessoa().getTelaPessoa().inicio();
-     * break; case (2):
-     * CtrlPrincipal.getInstancia().getCtrlSala().getTelaSala().inicio(); break;
-     * case (3):
-     * CtrlPrincipal.getInstancia().getCtrlAcesso().getTelaAcesso().inicio();
-     * break; case (4): this.getCtrlPrincipal().abreTelaInicial(); break;
-     * default: throw new IllegalArgumentException(); } } catch
-     * (IllegalArgumentException e) { System.out.println( "Opcao Invalida!
-     * Escolha uma opcao dentre das opcoes na lista ."); String[] args = null;
-     * this.inicio(); } finally { teclado.nextLine(); } /
-     */
+//    public void inicio() throws IllegalArgumentException, InputMismatchException {
+//        System.out.println("--- TELA ADM - GERENCIAL---");
+//        System.out.println(" ");
+//        System.out.println("1 - Cadastro de pessoas ");
+//        System.out.println("2 - Cadastro de salas ");
+//        System.out.println("3 - Listar acessos ");
+//        System.out.println("4 - Voltar ao menu principal ");
+//        System.out.println("");
+//
+//        try {
+//            int opcao = validador.recebeValorInteiro("Escolha a opcao desejada, insira o numero e tecle enter : ---");
+//
+//            switch (opcao) {
+//                case (1):
+//                    CtrlPrincipal.getInstancia().getCtrlPessoa().getTelaPessoa().inicio();
+//                    break;
+//                case (2):
+//                    CtrlPrincipal.getInstancia().getCtrlSala().getTelaSala().inicio();
+//                    break;
+//                case (3):
+//                    CtrlPrincipal.getInstancia().getCtrlAcesso().getTelaAcesso().inicio();
+//                    break;
+//                case (4):
+//                    this.getCtrlPrincipal().abreTelaInicial();
+//                    break;
+//                default:
+//                    throw new IllegalArgumentException();
+//            }
+//        } catch (IllegalArgumentException e) {
+//            System.out.println("Opcao Invalida! Escolha uma opcao dentre das opcoes na lista .");
+//            String[] args = null;
+//            this.inicio();
+//        } finally {
+//            teclado.nextLine();
+//        }
+//    }
     private class GerenciadorBotoes implements ActionListener {
 
         @Override
@@ -138,6 +163,9 @@ public class TelaSwingAdm extends JFrame {
             } else if (ae.getActionCommand() == "3") {
                 setVisible(false);
                 CtrlPrincipal.getInstancia().getCtrlAcesso().abreTelaSwingAcesso();
+            } else if (ae.getActionCommand() == "4") {
+                setVisible(false);
+                CtrlPrincipal.getInstancia().abreTelaInicial();
             }
         }
     }
